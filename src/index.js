e12c32d871fe89937d1a34e031bd416e54e7cd10
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-25 17:07:02 +0500

    Test program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import board from "./GenerateBoard";
+import knightMoves from "./KnightMoves";
 
-console.log(board);
+function prettyPrint(moves) {
+  let str = `You made it in ${moves.length - 1} moves! Here's your path:\n`;
+  moves.forEach((move) => {
+    str += `[${move.toString()}]\n`;
+  });
+  console.log(str);
+}
+
+prettyPrint(knightMoves([0, 0], [0, 0]));
+prettyPrint(knightMoves([0, 0], [1, 2]));
+prettyPrint(knightMoves([0, 0], [3, 3]));
+prettyPrint(knightMoves([3, 3], [0, 0]));
+prettyPrint(knightMoves([0, 0], [7, 7]));
